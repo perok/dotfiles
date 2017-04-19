cdf765f419f2d91f69472141dbb1b7788f06d2a2
Per Øyvind Kanestrøm <per.oyvind.kanestrom@sgfinans.no>
2017-04-19 23:01:05 +0200

    Fix tmux setup

---
+++ zsh/user/darwin.zsh
@@ -0,0 +1,10 @@
+
+# Java path setup
+export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
+
+alias java7='export JAVA_HOME=$JAVA_7_HOME'
+alias java8='export JAVA_HOME=$JAVA_8_HOME'
+
+export JAVA_HOME=$JAVA_8_HOME
+
--- zsh/user/linux.zsh
+++ zsh/user/linux.zsh
@@ -1,6 +1,6 @@
 
-# Use XDG spec
-export TMUX_TMPDIR="$XDG_RUNTIME_DIR"/tmux
+# Use XDG spec TODO
+# export TMUX_TMPDIR="$XDG_RUNTIME_DIR"/tmux
 
 # Base16 Shell TODO not working on konsole
 #BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
