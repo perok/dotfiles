0a6c96d1fed8d397ab43753725574ae30a229a2e
Per Øyvind Kanestrøm <per.oyvind.kanestrom@sgfinans.no>
2017-07-21 22:06:07 +0200

    Setup VI mode for ZSH

--- zsh/user/keybindings.zsh
+++ zsh/user/keybindings.zsh
@@ -1,6 +1,11 @@
+# Vi mode
+# http://zshwiki.org/home/zle/vi-mode
+#https://dougblack.io/words/zsh-vi-mode.html
+
 # Keybinding mode: e-> emacs, v-> vi
-# TODO use vi
-bindkey -e
+bindkey -v
+
+export KEYTIMEOUT=1 # ESC timeout time 10ms
 
 # Enable Ctrl-x-e to edit command line
 autoload -U edit-command-line
@@ -9,5 +14,28 @@ zle -N edit-command-line
 bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
 # Vi style:
-# bindkey -M vicmd v edit-command-line
+#bindkey -M vicmd v edit-command-line
+
+# Show VI mode
+precmd() { RPROMPT="" }
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]% %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS2"
+    zle reset-prompt
+}
+zle -N zle-keymap-select
+zle -N zle-line-init
+
+# Vi bindings
+# Use vim cli mode
+bindkey '^P' up-history
+bindkey '^N' down-history
+# backspace and ^h working even after
+# returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
 
