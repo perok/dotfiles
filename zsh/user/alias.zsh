1b19c4244f0efee463096551c0f18fd6bc1622d2
Per Øyvind Kanestrøm <per.oyvind.kanestrom@sgfinans.no>
2017-12-09 16:51:32 +0100

    Add Zsh alias file

---
+++ zsh/user/alias.zsh
@@ -0,0 +1,24 @@
+
+# `exa` utility {{{
+alias l='exa -bghl --group-directories-first --sort name --git'
+alias ll='l --tree --level=3'
+# }}}
+
+
+# Docker {{{
+# Kill all running containers.
+alias dockerkillall='docker kill $(docker ps -q)'
+
+# Delete all stopped containers.
+alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+
+# Delete all untagged images.
+alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+
+# Delete all stopped containers and untagged images.
+alias dockerclean='dockercleanc || true && dockercleani'
+# }}}
+
+# Scala {{{
+alias amm="amm --no-remote-logging"
+# }}}
