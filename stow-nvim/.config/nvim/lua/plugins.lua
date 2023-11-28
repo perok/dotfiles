24f739ab1247e7cd5eb8df7c13126825d447d9a1
Per Øyvind Løvik Kanestrøm <per.kanestrom@bekk.no>
2023-11-28 11:11:17 +0100

    Add spectre

--- stow-nvim/.config/nvim/lua/plugins.lua
+++ stow-nvim/.config/nvim/lua/plugins.lua
@@ -365,6 +365,18 @@ require('lazy').setup({
     },
   },
 
+  -- search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    },
+  },
+
   {
     'nvim-treesitter/nvim-treesitter',
     event = { "VeryLazy" },
