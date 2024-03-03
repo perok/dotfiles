6ab47d2291f581ae37171309c67f2ff2f4979e78
Per Øyvind Løvik Kanestrøm <per.kanestrom@bekk.no>
2024-03-03 16:22:09 +0100

    nixos: storage optimize

--- nixos-config/nixos/configuration.nix
+++ nixos-config/nixos/configuration.nix
@@ -251,6 +251,13 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
